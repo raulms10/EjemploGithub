2d3142d873e712839608c7372ddfb363a582000d
raulms10 <algo@gmail.com>
2017-02-21 15:40:02 -0500

    Fanalizando proyecto

--- PermisosRuntime/app/src/main/java/co/edu/udea/compumovil/permisosruntime/MainActivity.java
+++ PermisosRuntime/app/src/main/java/co/edu/udea/compumovil/permisosruntime/MainActivity.java
@@ -5,6 +5,8 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -23,7 +25,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class MainActivity extends AppCompatActivity {
 
     private TextView lblPermiso;
-    private LinearLayout layout;
+    private TextView lblContacto;
     private final int MY_PERMISSIONS = 100;
     private final int OPEN_CONTACT = 200;
     private final String str_permitido = "PERMITIDO";
@@ -36,25 +38,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         estado = getResources().getString(R.string.lblName); //Obtiene el String del Label debajo del boton
         lblPermiso = (TextView) findViewById(R.id.lbnPermiso);
-        layout = (LinearLayout) findViewById(R.id.activity_main);
+        lblContacto = (TextView) findViewById(R.id.lblContacto);
 
-        if(verificarPermisos())
+        if(verificarPermisos()) //verificar los permisos
             lblPermiso.setText(estado+" "+str_permitido);
         else
             lblPermiso.setText(estado+" "+str_denegado);
-
-
-
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void on_Click(View view){
 
-        if(verificarPermisos()) {
-            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-            startActivityForResult(intent, OPEN_CONTACT);
+        if(verificarPermisos()) { //verificar los permisos
+            lblPermiso.setText(estado+" "+str_permitido);
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI); //Creamos un Intent para abrir los contactos
+            startActivityForResult(intent, OPEN_CONTACT); //Lanzamos el Intent y se espera a que se seleccione uno
         }else
-            requestPermissions(new String[]{READ_CONTACTS}, MY_PERMISSIONS); //Solicitar permisos
+            requestPermissions(new String[]{READ_CONTACTS}, MY_PERMISSIONS); //Solicitamos los permisos para abrir los contactos
     }
 
     public boolean verificarPermisos(){
@@ -62,24 +62,28 @@ public class MainActivity extends AppCompatActivity {
             return true;
         if(checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED)
             return true;
-        if(shouldShowRequestPermissionRationale(READ_CONTACTS)){//Solicitar permisos cuando los permisos son negados por el usuario en ajustes.
+        /*if(shouldShowRequestPermissionRationale(READ_CONTACTS)){//Solicitar permisos cuando los permisos son negados por el usuario en ajustes.
             //Toast.makeText(this, "Los permisos son necesarios para poder usar la aplicación", Toast.LENGTH_SHORT).show();
             //requestPermissions(new String[]{READ_CONTACTS}, MY_PERMISSIONS); //Solicitar permisos
         }else{//Solicitar permisos la primera vez que se instalaló l aplicación
             //requestPermissions(new String[]{READ_CONTACTS}, MY_PERMISSIONS); //Solicitar permisos
-        }
+        }*/
         return false;
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(resultCode == Activity.RESULT_OK){
             switch (requestCode){
                 case OPEN_CONTACT:
-                    Toast.makeText(this, "Volvio de contactos", Toast.LENGTH_SHORT).show();
+                    Uri contactUri = data.getData(); //Obtenemos la información del contacto seleccionado
+                    Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+                    String nombre = "\n"+getResources().getString(R.string.lblContacto)+"\n";
+                    if (cursor.moveToFirst()){
+                        nombre = nombre + cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    }
+                    lblContacto.setText(nombre);
                     break;
-
             }
         }
 
@@ -91,12 +95,12 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode == MY_PERMISSIONS){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "Permisos aceptados", Toast.LENGTH_SHORT).show();
+                lblPermiso.setText(estado+" "+str_permitido);
                 Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
                 startActivityForResult(intent, OPEN_CONTACT);
             }
         }else{
-            Toast.makeText(this, "Permisos denegados", Toast.LENGTH_SHORT).show();
+            lblPermiso.setText(estado+" "+str_denegado);
         }
 
     }
